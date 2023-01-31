3ae9a6ed6919842b061e24628184147bc84cc6e2
Tishly <ekaetteessien27@gmail.com>
2023-01-31 21:16:46 +0100

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
 """A Basic Flask Application with one endpoint"""
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def home():
     """Endpoint that routes user to the homepage"""
-   return render_template("index.html")
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
