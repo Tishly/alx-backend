3cccebe3bd864a71c13d46f9ae5fce7c36f687e9
Tishly <ekaetteessien27@gmail.com>
2023-01-31 23:37:31 +0100

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,25 +4,24 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
-# babel = Babel(app)
+babel = Babel(app)
 
 
 @app.route("/")
 def home():
     """Endpoint that routes user to the homepage"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 class Config(object):
-    LANGUAGES = ["en", "fr"]
+    LANGUAGES = ['en', 'fr']
     TIMEZONE = "UTC"
 
 
-@babel.localselector
-def get_locale():
-    return request.accept_languages.best_match(Config['LANGUAGES'])
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 
-babel = Babel(app, local_selector="en", timezone_selector="UTC")
+# babel = Babel(app, local_selector="en", timezone_selector="UTC")
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
