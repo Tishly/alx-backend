4c30d4e3c7889447593694d1adc420260344bd56
Tishly <ekaetteessien27@gmail.com>
2023-02-21 05:57:11 +0100

    Hypermedia pagination in python

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,13 +6,13 @@ import math
 server = __import__('1-simple_pagination').Server
 
 
-def get_hyper(page: int = 1, page_size: int = 10) -> dict:
+def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
     """A function that takes two args nd returns key:value pairs
     """
     assert isinstance(page, int) and page > 0
     assert isinstance(page_size, int) and page_size > 0
 
-    # server = Server()
+    """ server = Server()
     dataset = server.dataset()
 
     start_index, end_index = index_range(page, page_size)
@@ -25,13 +25,14 @@ def get_hyper(page: int = 1, page_size: int = 10) -> dict:
                 "next_page": None,
                 "prev_page": None,
                 "total_pages": 0,
-        }
+        }"""
 
-    page_data = dataset[start_index:end_index]
-    total_pages = math.ceil(len(dataset) / page_size)
+    page_data = self.get_page(page=page, page_size=page_size)
+    # page_data = dataset[start_index:end_index]
+    total_pages = math.ceil(len(self.dataset()) / page_size)
 
-    next_page = page + 1 if end_index < len(dataset) else None
-    prev_page = page - 1 if start_index > 0 else None
+    next_page = page + 1 if page < total_pages else None
+    prev_page = page - 1 if page > 0 else None
 
     return {
             "page_size": len(page_data),
