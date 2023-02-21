ea60b17cd7ce3b0dd0a86911b3bb569dfd2f11bd
Tishly <ekaetteessien27@gmail.com>
2023-02-21 03:19:13 +0100

    Hypermedia pagination in python

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,8 +6,8 @@ import math
 def get_hyper(page: int = 1, page_size: int = 10) -> dict:
     """A function that takes two args nd returns key:value pairs
     """
-    assert isinstance(page, int) and page > 0, "page argument must be an integer greater than 0"
-    assert isinstance(page_size, int) and page_size > 0, "page_size argument must be an integer greater than 0"
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
 
     server = Server()
     dataset = server.dataset()
@@ -22,19 +22,19 @@ def get_hyper(page: int = 1, page_size: int = 10) -> dict:
                 "next_page": None,
                 "prev_page": None,
                 "total_pages": 0,
-                }
+        }
 
-        page_data = dataset[start_index:end_index]
-        total_pages = math.ceil(len(dataset) / page_size)
+    page_data = dataset[start_index:end_index]
+    total_pages = math.ceil(len(dataset) / page_size)
 
-        next_page = page + 1 if end_index < len(dataset) else None
-        prev_page = page - 1 if start_index > 0 else None
+    next_page = page + 1 if end_index < len(dataset) else None
+    prev_page = page - 1 if start_index > 0 else None
 
-        return {
-                "page_size": len(page_data),
-                "page": page,
-                "data": page_data,
-                "next_page": next_page,
-                "prev_page": prev_page,
-                "total_pages": total_pages,
-                }
+    return {
+            "page_size": len(page_data),
+            "page": page,
+            "data": page_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+    }
