84ac155440bfe8dabc28ebcc57b4e0c937aeeec2
Tishly <ekaetteessien27@gmail.com>
2023-02-21 01:51:32 +0100

    Simple helper function for pagination in python

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-""" """
+"""A simple helper pagination function in python"""
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """"""
+    """A function that returns the start and end index for pagination
+    Args:
+        Page -> integer indicating the first page
+        page_size -> integer indicating the number of pages to load/release"""
     start_index = page + page_size + 1
     end_index = start_index + page_size
     return (start_index, end_index)
